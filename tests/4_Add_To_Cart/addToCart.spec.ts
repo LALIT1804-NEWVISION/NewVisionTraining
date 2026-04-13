feda7551154999698ddbd31e24c827a98a5e9d1f
LALIT1804-NEWVISION <lalit.gupta@newvision-software.com>
2026-04-13 17:11:06 +0530

    AddToCart script

--- tests/4_Add_To_Cart/addToCart.spec.ts
+++ tests/4_Add_To_Cart/addToCart.spec.ts
@@ -20,10 +20,22 @@ test('TC02_AddToCart - Multiple products add', async ({ app }) => {
     await expect(app.cart.getCartBadge())
         .toHaveText(AddToCartData.cart.multipleCount.toString());
 });
+test('TC03_AddToCart - Verify cart badge count increases accordingly', async ({ app }) => {
+    
+    const products = AddToCartData.products.multiple;
 
-test('TC03_AddToCart - Cart badge increases', async ({ app }) => {
-    await app.cart.addProductByName(AddToCartData.products.single);
+    await app.cart.addProductByName(products[0]);
+    await expect(app.cart.getCartBadge()).toHaveText('1');
 
-    await expect(app.cart.getCartBadge())
-        .toHaveText(AddToCartData.cart.multipleCount.toString());
+    await app.cart.addProductByName(products[1]);
+    await expect(app.cart.getCartBadge()).toHaveText('2');
+});
+
+test('TC04_AddToCart - Cart badge increases dynamically', async ({ app }) => {
+    const products = AddToCartData.products.multiple;
+
+    for (let i = 0; i < products.length; i++) {
+        await app.cart.addProductByName(products[i]);
+        await expect(app.cart.getCartBadge()).toHaveText((i + 1).toString());
+    }
 });
